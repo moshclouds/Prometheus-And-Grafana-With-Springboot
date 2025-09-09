7def3bdd64ae2fed9e9dc3a76988fc766f984bb8
Mohammad Shabeer <moshdev2213@gmail.com>
2025-09-09 07:47:12 +0530

    Added Eureka Intergration

--- api-gateway/src/main/java/com/moshcloudsspringmetrics/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/moshcloudsspringmetrics/api_gateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.moshcloudsspringmetrics.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
